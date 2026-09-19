dba7daaf1ae5229fe49ad649dcbadc451178021d
Demsource <demolomsadze@gmail.com>
2026-09-20 03:24:28 +0400

    chore(config): update ESLint flat config with TypeScript, Unicorn, and strict rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,26 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierPlugin from 'eslint-plugin-prettier/recommended';
+import unicorn from 'eslint-plugin-unicorn';
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  unicorn.configs.recommended,
   prettierPlugin,
   {
+    linterOptions: {
+      noInlineConfig: true,
+    },
     languageOptions: {
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
     },
-    rules: {},
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'unicorn/prevent-abbreviations': 'off',
+    },
   }
 );
