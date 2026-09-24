6f2bed4e2c8523757ad4793605e2149f3a6630d8
Demsource <demolomsadze@gmail.com>
2026-09-25 01:21:20 +0400

    feat(library): implement section header

--- src/pages/library/library-page.ts
+++ src/pages/library/library-page.ts
@@ -1,5 +1,6 @@
 import { Header } from '../../components/header/header';
 import '../../components/header/header.scss';
+import { PageTitle } from '../../components/page-title/page-title';
 import { Footer } from '../../components/footer/footer';
 import './library-page.scss';
 
@@ -8,6 +9,12 @@ export function Library(currentRoute: string): HTMLElement {
   container.className = 'page-container';
 
   container.append(Header(currentRoute));
+  container.append(
+    PageTitle({
+      title: 'Game Library',
+      subtitle: 'Browse our collection of casual mini-games',
+    })
+  );
   container.append(Footer());
   return container;
 }
